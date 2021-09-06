309682d4addcde1562b415ead1f0390a25bd3377
mckenziev <87456641+mckenziev@users.noreply.github.com>
2021-09-06 15:18:18 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -8,7 +8,7 @@
         $user="root";
         $pass="";
 
-        $bd="clinical";
+        $bd="clinica";
 
         $con = mysqli_connect($host,$user,$pass);
         mysqli_select_db($con, $bd);
